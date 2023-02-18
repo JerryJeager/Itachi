b39237b3524ba08f3a2b505c4adc5645fcc4a32c
Amadi Jerry <amadijerry310@gmail.com>
2023-02-18 01:44:18 +0100

    added opening and closing of sidebar

---
+++ script.js
@@ -0,0 +1,11 @@
+const hamburger = document.querySelector('.hamburger')
+const closeSideBarBtn = document.querySelector('.close-side-bar')
+const nav = document.querySelector('.nav')
+
+
+hamburger.addEventListener('click', () => {
+    nav.classList.add('nav-show')
+})
+closeSideBarBtn.addEventListener('click', () => {
+    nav.classList.remove('nav-show')
+})
