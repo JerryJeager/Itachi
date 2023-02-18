04e22d407c41ff2549793e7bd34925d75081a7eb
Amadi Jerry <amadijerry310@gmail.com>
2023-02-18 04:26:32 +0100

    added hero images using swiper.js

--- script.js
+++ script.js
@@ -3,9 +3,13 @@ const closeSideBarBtn = document.querySelector('.close-side-bar')
 const nav = document.querySelector('.nav')
 
 
+
+
+
 hamburger.addEventListener('click', () => {
     nav.classList.add('nav-show')
 })
 closeSideBarBtn.addEventListener('click', () => {
     nav.classList.remove('nav-show')
 })
+
